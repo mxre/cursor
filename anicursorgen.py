e9e1ca78e826619cf6a9e3a39b8b230a387d2785
Max Resch <resch.max@gmail.com>
2019-07-31 02:57:13 +0200

    remove print_function from future in anigencursor

--- anicursorgen.py
+++ anicursorgen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # anicursorgen
 # Copyright (C) 2015 Руслан Ижбулатов <lrn1986@gmail.com>
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from __future__ import print_function
 
 import sys
 import os
